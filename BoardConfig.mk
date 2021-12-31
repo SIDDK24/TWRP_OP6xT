a968abab8d7404e3e6b332a6b227eb4120ff539c
Siddhesh Koyande <34239658+siddhrsh@users.noreply.github.com>
2021-12-31 20:33:51 +0530

    SHRPify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,3 +151,40 @@ TARGET_USES_LOGD := true
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+
+#SHRP Prop
+# Official 
+SHRP_OFFICIAL := true
+# NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC). 
+# Path of your SHRP Tree
+SHRP_PATH := device/oneplus/enchilada
+# Maintainer name *
+SHRP_MAINTAINER := SIDDK
+# Device codename *
+SHRP_DEVICE_CODE := enchilada
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := Treble
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A/B
+# SHRP Padding Flag (Only for rounded corner devices.) [Optional]
+# You have to change these values according to your device's roundness.
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_STATUSBAR_LEFT_PADDING := 40
+# For Notch devices [Optional]
+SHRP_NOTCH := true
+# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock [Optional]
+#SHRP_EXPRESS := true
+# SHRP Dark mode, use this flag to have dark theme set by default [Optional]
+SHRP_DARK := true
+# put this 0 if device has no EDL mode *
+SHRP_EDL_MODE := 1
+# Put your device's paths from fstab *
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+# Put 0 to disable flashlight *
+SHRP_FLASH := 1
+# Max Brightness of LED [Optional]
+SHRP_FLASH_MAX_BRIGHTNESS := 200
+# Use this flag only if your device is A/B *
+SHRP_AB := true
